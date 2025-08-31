c9751fee11fc7311d4c9f0a8df33a894373187bd
okitha_eske <164752460+okithaeske@users.noreply.github.com>
2025-08-31 14:26:03 +0530

    changed the netrance page

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'pages.home')->name('home');
-Route::view('/welcome', 'welcome')->name('welcome');
+Route::view('/', 'welcome')->name('welcome');
+Route::view('/home', 'pages.home')->name('home');
 
 // Public pages
 Route::view('/about', 'pages.about')->name('about');
